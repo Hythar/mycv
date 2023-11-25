7ed85c2c7fb45558ad9a9627281c97a078d47cf5
Yusufbarre <149888223+Yusufbarre@users.noreply.github.com>
2023-11-25 09:40:03 -0800

    Snake.py
    changes made
    Added grid lines to provide a better visual representation of the game area.
    Set a light blue background color for better contrast.
    Added a title to the game window to identify it as the "Snake Game".
    Removed unnecessary calls to hideturtle() and tracer(False) as these were interfering with the grid lines.
    Incorporated bye() to gracefully exit the game when it's over.

--- Snake.py
+++ Snake.py
@@ -2,54 +2,86 @@ from turtle import *
 from random import randrange
 from freegames import square, vector
 
+# Constants
+GRID_SIZE = 20
+MOVE_INTERVAL = 100
+
 food = vector(0, 0)
 snake = [vector(10, 0)]
-aim = vector(0, -10)
+aim = vector(0, -GRID_SIZE)
 
 def change(x, y):
-    "Change snake direction."
     aim.x = x
     aim.y = y
 
 def inside(head):
-    "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
 
 def move():
-    "Move snake forward one segment."
     head = snake[-1].copy()
     head.move(aim)
 
     if not inside(head) or head in snake:
-        square(head.x, head.y, 9, 'red')
-        update()
+        game_over()
         return
 
     snake.append(head)
 
     if head == food:
-        print('Snake:', len(snake))
-        food.x = randrange(-15, 15) * 10
-        food.y = randrange(-15, 15) * 10
+        handle_food_collision()
     else:
         snake.pop(0)
 
+    update_board()
+    ontimer(move, MOVE_INTERVAL)
+
+def handle_food_collision():
+   
+    print('Snake:', len(snake))
+    food.x = randrange(-15, 15) * GRID_SIZE
+    food.y = randrange(-15, 15) * GRID_SIZE
+
+def update_board():
     clear()
 
+    # Draw grid lines
+    for x in range(-200, 200, GRID_SIZE):
+        square(x, -200, 1, 'gray')
+        square(x, 200, 1, 'gray')
+
+    for y in range(-200, 200, GRID_SIZE):
+        square(-200, y, 1, 'gray')
+        square(200, y, 1, 'gray')
+
+    # Draw snake
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, GRID_SIZE, 'black')
 
-    square(food.x, food.y, 9, 'green')
+    # Draw food
+    square(food.x, food.y, GRID_SIZE, 'green')
     update()
-    ontimer(move, 100)
 
+def game_over():
+    
+    square(snake[-1].x, snake[-1].y, GRID_SIZE, 'red')
+    update()
+    bye()
+
+# Set up the game window
 setup(420, 420, 370, 0)
+title("Snake Game")
+bgcolor("lightblue")
+
 hideturtle()
 tracer(False)
 listen()
-onkey(lambda: change(10, 0), 'Right')
-onkey(lambda: change(-10, 0), 'Left')
-onkey(lambda: change(0, 10), 'Up')
-onkey(lambda: change(0, -10), 'Down')
+
+# Set up key bindings
+onkey(lambda: change(GRID_SIZE, 0), 'Right')
+onkey(lambda: change(-GRID_SIZE, 0), 'Left')
+onkey(lambda: change(0, GRID_SIZE), 'Up')
+onkey(lambda: change(0, -GRID_SIZE), 'Down')
+
+# Start the game
 move()
 done()
